761549358ed02a66c555932384faab90e9ba9131
Maxime <maximebarber@gmail.com>
2018-07-25 15:25:18 +0200

    css map

---
+++ view/carte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Carte</title>
+    <link rel="stylesheet" href="content/style.css">
+  </head>
+  <body>
+
+<div class="map">
+
+</div>
+
+<script type="text/javascript" src="ControlleurLeaflet.js"></script>
+
+  </body>
+</html>
