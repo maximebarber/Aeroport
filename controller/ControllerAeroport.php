9f9a7e19c371d09eb4189a5bde0adb466fae9d54
Maxime <maximebarber@gmail.com>
2018-07-25 15:48:44 +0200

    maj

--- controller/ControllerAeroport.php
+++ controller/ControllerAeroport.php
@@ -4,16 +4,11 @@ require_once 'model/Aeroport.php';
 
 function addAeroport($data) {
 
-    $code_aeroport = $data['code_aeroport'];
-    $id_ville      = $data['id_ville'];
-    $latitude      = $data['latitude'];
-    $longitude     = $data['longitude'];
-    $nom_aeroport  = $data['nom_aeroport'];
 
     $aeroport = new Aeroport();
 
-    $aeroport->addAeroport($code_aeroport, $id_ville, $latitude, $longitude, $nom_aeroport);
-
+    $aeroport->addAeroport($data);
+    echo "sdsd";
     require_once 'view/addAeroport.php';
 
 }
--- model/Aeroport.php
+++ model/Aeroport.php
@@ -4,23 +4,23 @@ require_once 'model/Model.php';
 
 class Aeroport extends Model {
 
-    //Connection à la bdd 
+    //Connection à la bdd
     public function __construct() {
         parent::getBdd();
     }
 
     //Ajout d'un aéroport à la bdd
-    public function addAeroport($code_aeroport, $id_ville, $latitude, $longitude, $nom_aeroport) {
+    public function addAeroport($data) {
 
         $sql = 'INSERT INTO AEROPORT (code_aeroport, id_ville, latitude, longitude, nom_aeroport)
                 VALUES (:code_aeroport, :id_ville, :latitude, :longitude, :nom_aeroport)';
 
         $req = $this->bdd->prepare($sql);
-        $req->execute(array(':code_aeroport' => $code_aeroport,
-                            ':id_ville'      => $id_ville,
-                            ':latitude'      => $latitude,
-                            ':longitude'     => $longitude,
-                            ':nom_aeroport'  => $nom_aeroport));
+        $req->execute(array(':code_aeroport' => $data['code_aeroport'],
+                            ':id_ville'      => $data['id_ville'],
+                            ':latitude'      => $data['latitude'],
+                            ':longitude'     => $data['longitude'],
+                            ':nom_aeroport'  => $data['nom_aeroport']));
 
         return $req;
 
